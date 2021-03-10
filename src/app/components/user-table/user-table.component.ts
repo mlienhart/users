80398cc0943b3c5c7f6d55f1f090af4e9c34f61d
Martin Lienhart <martin.lienhart@outlook.com>
2021-03-10 22:36:41 +0100

    31

--- src/app/components/user-table/user-table.component.ts
+++ src/app/components/user-table/user-table.component.ts
@@ -1,18 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { User } from 'src/app/interfaces/user';
 import { UserService } from 'src/app/services/user.service';
+import { USERS } from 'src/app/USERS';
 
 @Component({
   selector: 'app-user-table',
   templateUrl: './user-table.component.html',
   styleUrls: ['./user-table.component.css']
 })
-export class UserTableComponent implements OnInit {
+export class UserTableComponent implements AfterViewInit {
+  dataSource: MatTableDataSource<User>;
 
-  constructor(private userService: UserService) { }
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit(): void {
     this.getUsers();
+  }
+
+  constructor(private userService: UserService) {
+    this.dataSource = new MatTableDataSource(USERS);
   }
 
   users: User[] = [];
@@ -24,4 +34,17 @@ export class UserTableComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'birthday', 'sex', 'risk', 'card', 'registered', 'points'];
 
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
